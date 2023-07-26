f539046c5d0d8e405daa93faa73cc93c0a3124fd
Gabriel Madruga <gabrielsantosmadruga@gmail.com>
2023-07-26 20:43:53 -0300

    [FEATURE] Add some logging and docstrings

--- database/create_table.py
+++ database/create_table.py
@@ -9,15 +9,19 @@ with open(config_dir+"/config.json", 'r') as arquivo:
 
 
 def read_query_from_file(file_path):
+    """ 
+    This function retrieves a query from the path received as input
+    """
     with open(file_path, 'r') as file:
         query = file.read()
     return query
 
 
-# Função para criar a tabela
 def create_table(query):
+    """
+    This function executes the query, creating the table in the database
+    """
     try:
-        # Conecta ao banco de dados
         connection = psycopg2.connect(
             host=connection_args["POSTGRES_HOST"],
             port=connection_args["POSTGRES_PORT"],
@@ -26,13 +30,8 @@ def create_table(query):
             password=connection_args["POSTGRES_PASSWORD"]
         )
         
-        # Cria um cursor para executar comandos SQL
         cursor = connection.cursor()
-        
-        # Executa o comando para criar a tabela
         cursor.execute(query)
-        
-        # Confirma a transação
         connection.commit()
         
         print("Tabela criada com sucesso!")
@@ -41,12 +40,10 @@ def create_table(query):
         print("Erro ao criar a tabela:", error)
         
     finally:
-        # Fecha o cursor e a conexão
         if cursor:
             cursor.close()
         if connection:
             connection.close()
 
-# Chama a função para criar a tabela
 query = read_query_from_file(current_dir+"/query/create_brt_raw_data_table.sql")
 create_table(query)
--- pipeline/tasks.py
+++ pipeline/tasks.py
@@ -9,21 +9,41 @@ import fcntl
     
 
 @task
-def make_api_request(api_url):
+def make_api_request(api_url: str, max_retries=3, retry_delay=1):
+    """
+    Performs the request to the API and returns the received value
+    """
+
     logger = prefect.context.get("logger")
-    response = requests.get(api_url)
-    return response.json()
+    attempts = 0
+    while attempts < max_retries:
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()  
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            logger.warning(f"Request error: {e}")
+            attempts += 1
+            if attempts < max_retries:
+                logger.info(f"Trying again in {retry_delay} seconds...")
+                time.sleep(retry_delay)
+    
+    logger.error("Maximum number of attempts reached. The request failed.")
+    return None
 
 
 @task 
 def storage_data(source_data: str, destination_path: str):
+    """
+    Performs local data storage
+    """
+
     logger = prefect.context.get("logger")
-    
     try:
         with open(destination_path, 'r') as file:
             while is_file_locked(file):
-                print(f"Arquivo {destination_path} em uso, aguardando liberação...")
-                time.sleep(2)
+                logger.info(f"File {destination_path} in use, waiting for release...")
+                time.sleep(5)
 
             fcntl.flock(file.fileno(), fcntl.LOCK_EX)
 
@@ -40,6 +60,7 @@ def storage_data(source_data: str, destination_path: str):
             logger.info(f"Data saved in {destination_path}")
     except:
         logger.warning("No saved data")
+        logger.info("Creating new .csv ...")
         source_data = source_data.get("veiculos")
         result_df = pd.DataFrame(source_data)
         result_df.to_csv(destination_path)
@@ -47,7 +68,9 @@ def storage_data(source_data: str, destination_path: str):
 
 @task 
 def send_data_to_postgres(local_data: pd.DataFrame, connection_args: dict):
-
+    """
+    Send the local data to Postgres
+    """
     user = connection_args["POSTGRES_USER"]
     password = connection_args["POSTGRES_PASSWORD"]
     host = connection_args["POSTGRES_HOST"]
@@ -62,26 +85,23 @@ def send_data_to_postgres(local_data: pd.DataFrame, connection_args: dict):
     table_name = 'brt_raw'
     local_data.to_sql(table_name, engine, if_exists='append', index=False)
 
-    local_data.to_csv("postgresql_data.csv")
-
     pass
 
     
 @task
-def read_local_data(csv_filename):
-
-    # Adquire uma trava exclusiva no arquivo .csv
+def read_local_data(csv_filename: str):
+    """
+    Performs local data reading
+    """
+    logger = prefect.context.get("logger")
     with open(csv_filename, 'r') as file:
         while is_file_locked(file):
-            print(f"Arquivo {csv_filename} em uso, aguardando liberação...")
-            time.sleep(2)
+            logger.info(f"File {csv_filename} in use, waiting for release...")
+            time.sleep(5)
         fcntl.flock(file.fileno(), fcntl.LOCK_EX)
-
         df = pd.read_csv(csv_filename,index_col=0)
         new_df = pd.DataFrame(columns=df.columns)
         new_df.to_csv(csv_filename)
-
-        # Libera a trava do arquivo
         fcntl.flock(file.fileno(), fcntl.LOCK_UN)
-
+    logger = prefect.context.get("Local data has been read successfully")
     return df
